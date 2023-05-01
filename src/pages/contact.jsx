613e141fa45cca699369a8b6915554c077c79b1f
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2023-05-01 13:04:13 -0400

    added updated resume and form reset after submit

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -35,59 +35,76 @@ export default function Contact() {
 
   const sendEmail = (e) => {
     e.preventDefault()
-    emailjs.sendForm('service_o727s6s', 'template_s8452k9', form.current, '7kWBzm_WbavGXPlM1')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        'service_o727s6s',
+        'template_s8452k9',
+        form.current,
+        '7kWBzm_WbavGXPlM1'
+      )
+      .then(
+        (result) => {
           console.log(result.text)
           console.log('Message sent')
           toast.success('Message sent!', {
-            position: "top-center",
+            position: 'top-center',
             autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "dark",
-            });
+            theme: 'dark',
+          })
+          // Reset the form
+          form.current.reset();
         },
         (error) => {
           console.log(error.text)
           toast.error('Error: something went wrong!', {
-            position: "top-center",
+            position: 'top-center',
             autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "dark",
-            });
+            theme: 'dark',
+          })
         }
       )
   }
 
   return (
     <>
-    <Head>
-      <title>Contact - Jonathan Yazdanpanah</title>
-      <meta
-        name="description"
-        content="Let's Make Something Amazing Together." />
-    </Head><SimpleLayout
-      title="Let's Make Something Amazing Together."
-      intro={<>
-        {"If you're an employer looking to hire a skilled front-end developer, I'd love to hear from you. Simply fill out the form below, or if you prefer, you can also email me directly at "}{' '}
-        <a
-          className="text-teal-500"
-          href="mailto:jon.yazdan@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          jon.yazdan@gmail.com
-        </a>
-        {". Whether you're looking to discuss a project, explore new ideas, or just want to say hello, I'm here and ready to listen."}
-      </>}
-    >
+      <Head>
+        <title>Contact - Jonathan Yazdanpanah</title>
+        <meta
+          name="description"
+          content="Let's Make Something Amazing Together."
+        />
+      </Head>
+      <SimpleLayout
+        title="Let's Make Something Amazing Together."
+        intro={
+          <>
+            {
+              "If you're an employer looking to hire a skilled front-end developer, I'd love to hear from you. Simply fill out the form below, or if you prefer, you can also email me directly at "
+            }{' '}
+            <a
+              className="text-teal-500"
+              href="mailto:jon.yazdan@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              jon.yazdan@gmail.com
+            </a>
+            {
+              ". Whether you're looking to discuss a project, explore new ideas, or just want to say hello, I'm here and ready to listen."
+            }
+          </>
+        }
+      >
         <form
           ref={form}
           onSubmit={sendEmail}
@@ -105,7 +122,8 @@ export default function Contact() {
               placeholder="Name"
               aria-label="Name"
               required
-              className="mb-5 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm" />
+              className="mb-5 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+            />
             <input
               type="email"
               // Name as to be the same as email.js template
@@ -113,7 +131,8 @@ export default function Contact() {
               placeholder="Email address"
               aria-label="Email address"
               required
-              className="mb-5 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm" />
+              className="mb-5 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+            />
             <textarea
               type="message"
               // Name as to be the same as email.js template
@@ -121,7 +140,8 @@ export default function Contact() {
               placeholder="Message"
               aria-label="Message"
               required
-              className="mb-6 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] pb-20 shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm" />
+              className="mb-6 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] pb-20 shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+            />
             <Button
               type="submit"
               className="mt-4 flex w-full items-center justify-center lg:w-1/6"
@@ -130,6 +150,7 @@ export default function Contact() {
             </Button>
           </div>
         </form>
-      </SimpleLayout></>
+      </SimpleLayout>
+    </>
   )
 }
